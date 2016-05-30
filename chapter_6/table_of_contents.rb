c33b0ede43f81069534d532760e0c617695e63a8
Kyle <kyle.m.bowen@gmail.com>
2016-05-30 16:31:31 -0700

    Finish and fix table_of_contents.rb

--- chapter_6/table_of_contents.rb
+++ chapter_6/table_of_contents.rb
@@ -1,4 +1,4 @@
-line_width = 80
+line_width = 60
 heading = 'Table of Contents'
 chapter_number_str = 'Chapter 1:  '
 chapter_title = 'Getting Started'
@@ -7,4 +7,5 @@ page_num = 'page  1'
 puts(heading.center(line_width))
 puts
 puts((chapter_number_str + chapter_title).ljust(line_width/2) + page_num.rjust(line_width/2))
-puts(("Chapter 2:  Numbers").ljust(line_width) + "page  9".rjust(line_width/2))
+puts(("Chapter 2:  Numbers").ljust(line_width/2) + "page  9".rjust(line_width/2))
+puts(("Chapter 3:  Letters").ljust(line_width/2) + "page 13".rjust(line_width/2))
