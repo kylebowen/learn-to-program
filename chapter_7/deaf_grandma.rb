3595d834fd127d530ed08252ec94ddb33b5575a2
Kyle <kyle.m.bowen@gmail.com>
2016-06-21 20:33:20 -0700

    Add deaf_grandma.rb from Chapter 7.5

---
+++ chapter_7/deaf_grandma.rb
@@ -0,0 +1,14 @@
+puts "Thanks for coming to talk with your dear Grandma!".upcase
+
+while true
+  input = gets.chomp
+  break if input == "BYE"
+
+  if input == input.upcase
+    puts "No, not since #{1930 + rand(21)}!".upcase
+  else
+    puts "Huh?! Speak up, Sonny!".upcase
+  end
+end
+
+puts "Talk to you later! Bye!".upcase
